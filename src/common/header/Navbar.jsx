844a4430edd1c4eb32d6d75935233d69fc2ea408
Liam7-Denin <119574335+Liam7-Denin@users.noreply.github.com>
2023-02-26 00:46:21 +0530

    Add files via upload

---
+++ src/common/header/Navbar.jsx
@@ -0,0 +1,59 @@
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+
+const Navbar = () => {
+  const [MobileMenu, setMobileMenu] = useState(false);
+  return (
+    <>
+      <header className="header">
+        <div className="container d_flex">
+          <div className="catgrories d_flex">
+            <span class="fa-solid fa-border-all"></span>
+            <h4>Categories</h4>
+          </div>
+
+          <div className="navlink">
+            <ul
+              className={
+                MobileMenu ? "nav-links-MobileMenu" : "link f_flex capitalize"
+              }
+              onClick={() => setMobileMenu(false)}
+            >
+              <li>
+                <Link to="/">home</Link>
+              </li>
+              <li>
+                <Link to="/pages">pages</Link>
+              </li>
+              <li>
+                <Link to="/form">user account</Link>
+              </li>
+              <li>
+                <Link to="/vendor">vendor account</Link>
+              </li>
+              <li>
+                <Link to="/track">track my order</Link>
+              </li>
+              <li>
+                <Link to="/contact">contact</Link>
+              </li>
+            </ul>
+
+            <button
+              className="toggle"
+              onClick={() => setMobileMenu(!MobileMenu)}
+            >
+              {MobileMenu ? (
+                <i className="fas fa-times close home-btn"></i>
+              ) : (
+                <i className="fas fa-bars open"></i>
+              )}
+            </button>
+          </div>
+        </div>
+      </header>
+    </>
+  );
+};
+
+export default Navbar;
---
+++ src/components/MainPage/Sdata.js
@@ -0,0 +1,27 @@
+const Sdata = [
+  {
+    id: 1,
+    title: "50% Off For Your First Shopping",
+    desc: "Apple Watch Series 7 (GPS + Cellular, 41mm) - Midnight Aluminium Case with Midnight Sport Band - Regular",
+    cover: "./images/SlideCard/slide-1.png",
+  },
+  {
+    id: 2,
+    title: "50% Off For Your First Shopping",
+    desc: "Nike Mens Air Zoom Structure 24 Running Shoe",
+    cover: "./images/SlideCard/slide-2.png",
+  },
+  {
+    id: 3,
+    title: "50% Off For Your First Shopping",
+    desc: "Columbia Men's Polyester Track Jacket (WM3220_Large_Black)",
+    cover: "./images/SlideCard/slide-3.png",
+  },
+  {
+    id: 4,
+    title: "50% Off For Your First Shopping",
+    desc: "Pink Cherry Blossom CZ Clip 925 Sterling Silver Bead Love Heart Charm Clip Charm Lock Stopper Charm Spacer Charm for Pandora Charms Bracelet (Pink Cherry Blossom Charms)",
+    cover: "./images/SlideCard/slide-4.png",
+  },
+];
+export default Sdata;
